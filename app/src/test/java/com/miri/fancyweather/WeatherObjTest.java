a162710ed589b0d1169e2c90088e35e656553ca3
Miriam <ente1993@yahoo.de>
2017-01-09 16:44:48 +0100

    Add first test for WeatherObj Rename example test file and reuse it later to test the networking

---
+++ app/src/test/java/com/miri/fancyweather/WeatherObjTest.java
@@ -0,0 +1,21 @@
+package com.miri.fancyweather;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Created by miri on 09.01.17.
+ */
+
+public class WeatherObjTest {
+
+    @Test
+    public void testWeatherObj() throws Exception {
+        WeatherObj TestWeatherObj = new WeatherObj("clear sky" , "5" , "Berlin");
+        assertEquals(TestWeatherObj.getWeather() , "clear sky");
+        assertEquals(TestWeatherObj.getTemperature() , "5");
+        assertEquals(TestWeatherObj.getLocation() , "Berlin");
+    }
+
+}
+
